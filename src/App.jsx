f95a17d1ee34e2cb57020a7148fcd4fbd5c56607
Abdullah Güzel <113038939+vlocers@users.noreply.github.com>
2024-07-30 19:47:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import {Route,Routes} from "react-router-dom"
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Menu from './components/Menu'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    <div className="App">
-      <Navbar/>
-      <Routes>
-        <Route path='/' exact element={<Home/>}/>
-        <Route path='/menu' exact element={<Menu/>}/>
-        <Route path='/about' exact element={<About/>}/>
-        <Route path='/contact' exact element={<Contact/>}/>
-        
-      </Routes>
-      <Footer/>
-    </div>
-      
-    </>
-  )
+    <BrowserRouter basename="/my-react-app"> {/* Depo adınızı burada belirtin */}
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/menu' exact element={<Menu />} />
+          <Route path='/about' exact element={<About />} />
+          <Route path='/contact' exact element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
